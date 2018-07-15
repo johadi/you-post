d10d2748764d4efaadd4454e1329636f898ec261
johadi <jimoh.hadi@gmail.com>
2018-07-15 14:47:32 +0100

    feat(group): implement `view group messages` functionality

--- src/app/pages/group-messages/group-messages.component.ts
+++ src/app/pages/group-messages/group-messages.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GroupService } from '../../services/group.service';
 
 @Component({
   selector: 'app-group-messages',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupMessagesComponent implements OnInit {
 
-  constructor() { }
+  groupMessages: any[];
+  constructor(private route: ActivatedRoute, private groupService: GroupService) { }
 
   ngOnInit() {
+    this.getGroupMessages();
+  }
+
+  getGroupMessages() {
+    const groupId = this.route.parent.snapshot.paramMap.get('id');
+    this.groupService.getGroupMessages(groupId)
+      .toPromise()
+      .then(response => {
+        console.log('+++++++++', response.rows);
+        this.groupMessages = response.rows;
+      })
+      .catch(err => console.log(err));
   }
 
 }
